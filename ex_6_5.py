bed19ca27bc96d93e2c9a40899e409c99a9e7f92
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-05-01 23:51:16 +0300

    Update ex_6_5.py

--- ex_6_5.py
+++ ex_6_5.py
@@ -1,19 +1,14 @@
-from _ctypes_test import func
 
 
-def group_by(function, iterable) :
+def group_by(function, iterable):
     """
     A function that gets an iterable,
-    :param func:The function that goes with the iterable.
-    :param iterable:The iterable that we use.
-    :return:A dictionary whose keys are the values that we get ,
+    :param function: The function that goes with the iterable.
+    :param iterable: The iterable that we use.
+    :return: A dictionary whose keys are the values that we get.
     """
 
+    return {function(index): list(filter(lambda term: function(term) == function(index), iterable))for index in iterable}
 
 
 
-    group={function(index): list(filter(lambda term: function(term) == function(index), iterable))for index in iterable}
-
-
-
-    return group;
