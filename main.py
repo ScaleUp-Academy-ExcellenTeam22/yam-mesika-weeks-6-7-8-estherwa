4ea6a22009c778eefd77c410511d5eea56f70f2b
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-06 15:06:08 +0300

    6.5 ready for submitting

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-
-# Press the green button in the gutter to run the script.
 from ex_6_4 import get_the_image_decryption
 from ex_6_5 import group_by
 
 if __name__ == '__main__':
+    # EX6_4
+    print(get_the_image_decryption('resources/code.png'))
 
-    #EX6_4
-    #print( get_the_image_decryption('resources/code.png'))
-
-    #EX6_5
-    main_list=["hi", "bye", "yo", "try"]
+    # EX6_5
+    main_list = ["hi", "bye", "yo", "try"]
     print(group_by(len, main_list))
-
